94632e6d4bacf292a9dbffa83790a9d5ec435f80
unknown <FISC@.(none)>
2011-10-01 12:24:46 -0500

    Subiendo el codigo :D

---
+++ app.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('fondo');
+var ctx = canvas.getContext('2d');
+
+var assetManager = new AssetManager();
+assetManager.queueDownload("./imagenes/vaca_normal.png","vacanormal");
+
+assetManager.downloadAll(function() {
+    var juego = new Juego(ctx);
+    var circulo = new Circulo(juego, 0, 225);
+    juego.addEntidad(circulo);
+    juego.iniciar();
+});
+
